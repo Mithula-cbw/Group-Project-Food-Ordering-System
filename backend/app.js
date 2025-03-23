571128666bc551784dc2ce434463094bcf01c05d
John Doe <dsk.sandeep.987@gmail.com>
2025-03-23 18:02:45 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -24,11 +24,19 @@ app.use(express.json());
 const userRoutes = require("./routes/user.js");
 const categoryRoutes = require("./routes/categories");
 const productRoutes = require("./routes/product");
+const cart = require("./routes/cart.js");
+const productReviews = require("./routes/productReview.js");
+const myList = require("./routes/myList.js");
+const checkout = require("./routes/checkout.js");
 
 app.use(`/uploads`, express.static("uploads"));
 app.use(`/api/category`, categoryRoutes);
 app.use(`/api/products`, productRoutes);
 app.use(`/api/user`, userRoutes);
+app.use(`/api/cart`, cart);
+app.use(`/api/productReviews`, productReviews);
+app.use(`/api/myList`, myList);
+app.use(`/api/checkout`, checkout);
 
 // Connect to MongoDB
 mongoose
